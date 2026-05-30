076cb03bd808c4df56a715cf2118c69bdae1f062
LiHao <3350535202@qq.com>
2026-05-30 14:43:43 +0800

    搜索功能

--- src/test/java/FindConferenceTest.java
+++ src/test/java/FindConferenceTest.java
@@ -3,6 +3,8 @@ import com.demo.web_project.dao.impl.ConferenceDaoImpl;
 import com.demo.web_project.vo.Conference;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class FindConferenceTest {
     private ConferenceDao conferenceDao= new ConferenceDaoImpl();
     @Test
@@ -11,7 +13,13 @@ public class FindConferenceTest {
         Conference conference=conferenceDao.findByCodes(code);
         System.out.println(conference.getTitle());
     }
+    @Test
     public  void testkeyword(){
-
+        String keyword="会" ;
+        List<Conference> conferenceList=conferenceDao.findAll(keyword);
+        for (int i = 0; i < conferenceList.size(); i++) {
+            Conference conference = conferenceList.get(i);
+            System.out.println(conference.getTitle());
+        }
     }
 }
